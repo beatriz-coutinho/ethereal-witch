e6c7a9da215f855f7f42592582dd490fa6462591
stun106 <bladingnordeste@gmail.com>
2024-06-16 04:40:38 -0300

    :sparkles: feat: recurso de buscar todos os clientes

--- src/data/Hooks/Cliente.tsx
+++ src/data/Hooks/Cliente.tsx
@@ -5,6 +5,7 @@ import { Users } from "../Types/User"
 export const useClient = () => {
 
    const [cliente, setClient] = useState<Users | undefined>(undefined)
+   const [ allCliente, setAllClient] = useState<Users[] | undefined>(undefined)
 
    const createClient = useCallback(async (clientData:Users) => {
         const { status } = await userService.createUser(clientData)
@@ -15,12 +16,19 @@ export const useClient = () => {
         const { data, status} = await userService.getUserById(id);
         if (status !== 200) throw new Error('Erro ao buscar dados da Api...');
         setClient(data);
-        return data;
     },[]);
+
+    const getAllClient = useCallback(async () => {
+        const { data, status } = await userService.getAllUser();
+        if (status !== 200) throw new Error('Erro ao buscar dados da Api...');
+        setAllClient(data);
+    },[])
 
     return {
         cliente,
+        allCliente,
         createClient,
-        getClientById
+        getClientById,
+        getAllClient,
     }
 }
--- src/data/Service/UserService.ts
+++ src/data/Service/UserService.ts
@@ -3,8 +3,10 @@ import { Users } from '../Types/User';
 
 const createUser = (data:Users) => Axios.post('/user/create',data);
 const getUserById = (id:number) => Axios.get(`/single/${id}`);
+const getAllUser = () => Axios.get('/all/auth');
 
 export const userService = {
     createUser,
     getUserById,
+    getAllUser,
 }
